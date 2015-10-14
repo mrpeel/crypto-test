a8eb5a569446b7616eff5f2388cfdaf267a4e66e
mrpeel <mrpeel@gmail.com>
2015-10-14 17:00:14 +1100

    Implements PBKDF2 and AES
    Full implementation using subtle crypto and falling back to CryptoJS

--- manager.js
+++ manager.js
@@ -1,14 +1,17 @@
-/*global  console, CryptoJS, Uint8Array, Promise, performance */
+/*global  console, CryptoJS, Uint8Array, Promise, performance, TextEncoder, TextDecoder, window, webcrypto, crypto, CryptoFunctions */
+
+var ns = "cake.man.io";
+var passphrase = "My Special Pass phrase";
+var domain = "mrpeel@mydomain.com.au";
+var plainText = "I hëart årt and £$¢!"; //"My secret message";
+var seedHex1, seedHex2, seedHex3, seedHex4;
 
 function runCrypto() {
-    var ns = "cake.man.io";
-    var passphrase = "My Test Password";
-    var domain = "mrpeel@gmail.com";
     var seed, seedArray;
     var t0 = performance.now();
-    
 
-    console.log("Pass phrase: " + passphrase);
+
+    console.log("CryptoJS ->Pass phrase: " + passphrase);
     console.log("Salt: " + ns);
     console.log("Domain: " + domain);
 
@@ -20,20 +23,193 @@ function runCrypto() {
     //console.log('Hex key: ' + derivedKey);
 
     return PBKDF2(passphrase, ns)
-    .then(function(key){
-        console.log('Derived key: ' + key);
+        .then(function (key) {
+            var keyArray = convertWordArrayToUint8Array(key);
+            console.log('Key array: ' + keyArray);
+            console.log('Key hex: ' + convertUint8ArrayToHex(keyArray));
 
-        return CryptoJS.HmacSHA256(domain, key);
-    }).then(function(seed){
-        console.log('HMAC result seed hex: ' + seed);
-        var seedArray =  convertWordArrayToUint8Array(seed);
+            return CryptoJS.HmacSHA256(domain, key);
+        }).then(function (seed) {
+            console.log('HMAC result seed hex: ' + seed);
+            var seedArray = convertWordArrayToUint8Array(seed);
+            seedHex1 = convertUint8ArrayToHex(seedArray);
 
-        console.log('HMAC result seed array: ' + seedArray);
-        console.log("All done"); 
-        console.log(performance.now() - t0 + " ms");
-    });
-    
+            console.log('HMAC result seed array: ' + seedArray);
+            console.log("All done");
+            console.log(performance.now() - t0 + " ms");
+        });
 }
+
+function useClassCryptoJS() {
+    var cryptoFunctions = new CryptoFunctions();
+    cryptoFunctions.useSubtle = false;
+
+
+    console.log("Class Crypto JS -> Pass phrase: " + passphrase);
+    console.log("Salt: " + ns);
+    console.log("Domain: " + domain);
+
+
+
+    cryptoFunctions.PBKDF2(passphrase, ns, 750, 128)
+        .then(function (key) {
+            console.log('Key hex: ' + cryptoFunctions.convertDerivedKeyToHex(key));
+
+            return cryptoFunctions.HMACSHA256(domain, key);
+        }).then(function (seed) {
+            seedHex3 = cryptoFunctions.convertUint8ArrayToHex(seed);
+            console.log('HMAC result seed hex: ' + seedHex3);
+
+            console.log('HMAC result seed array: ' + seed);
+            console.log("All done");
+        });
+
+}
+
+function useSubtleCrypto() {
+
+    console.log("Subtle -> Pass phrase: " + passphrase);
+    console.log("Salt: " + ns);
+    console.log("Domain: " + domain);
+
+
+    var cryptoTextEncoder = new TextEncoder("utf-8");
+    var cryptoTextDecoder = new TextDecoder("utf-8");
+
+    // salt should be Uint8Array or ArrayBuffer
+    var saltBuffer = cryptoTextEncoder.encode(ns);
+
+    var passphraseKey = cryptoTextEncoder.encode(passphrase);
+
+    var domainBuffer = cryptoTextEncoder.encode(domain);
+
+    crypto.subtle.importKey('raw', passphraseKey, {
+            name: 'PBKDF2'
+        }, false, ['deriveBits']).then(function (key) {
+            return crypto.subtle.deriveBits({
+                name: 'PBKDF2',
+                iterations: 750,
+                salt: saltBuffer,
+                hash: 'SHA-1'
+            }, key, 128);
+        }).then(function (derivedKey) {
+            var uint8Key = new Uint8Array(derivedKey);
+            console.log('Derived key hex: ' + convertUint8ArrayToHex(uint8Key));
+
+            return crypto.subtle.importKey("raw", derivedKey, {
+                name: "HMAC",
+                hash: {
+                    name: "SHA-256"
+                }
+            }, false /*not extractable*/ , ["sign"]);
+        })
+        .then(function (importedKey) {
+            return window.crypto.subtle.sign({
+                name: "HMAC",
+                hash: {
+                    name: "SHA-256"
+                }
+            }, importedKey, domainBuffer);
+        })
+        .then(function (seed) {
+            var seedArray = new Uint8Array(seed);
+            seedHex2 = convertUint8ArrayToHex(seedArray);
+            console.log('HMAC result seed hex: ' + seedHex2);
+            console.log('HMAC result seed array:' + seedArray);
+
+        });
+}
+
+function useClassSubtleCrypto() {
+    var cryptoFunctions = new CryptoFunctions();
+
+
+    console.log("Class Subtle Crypto -> Pass phrase: " + passphrase);
+    console.log("Salt: " + ns);
+    console.log("Domain: " + domain);
+
+
+
+    cryptoFunctions.PBKDF2(passphrase, ns, 750, 128)
+        .then(function (key) {
+            console.log('Derived key hex: ' + cryptoFunctions.convertDerivedKeyToHex(key));
+
+            return cryptoFunctions.HMACSHA256(domain, key);
+        }).then(function (seed) {
+            seedHex4 = cryptoFunctions.convertUint8ArrayToHex(seed);
+            console.log('HMAC result seed hex: ' + seedHex4);
+
+            console.log('HMAC result seed array: ' + seed);
+            console.log("SeedHex1: " + seedHex1);
+            console.log("SeedHex2: " + seedHex2);
+            console.log("SeedHex3: " + seedHex3);
+            console.log("SeedHex4: " + seedHex4);
+            console.log("SeedHex1 = SeedHex2 = SeedHex3 = SeedHex4: " + (seedHex1 === seedHex2 && seedHex2 === seedHex3 && seedHex3 === seedHex4));
+            console.log("All done");
+        });
+
+}
+
+function encryptDecryptCryptoJS() {
+    var cryptoFunctions = new CryptoFunctions();
+    cryptoFunctions.useSubtle = false;
+
+    var keyHolder;
+
+    console.log("Class Crypto JS -> Pass phrase: " + passphrase);
+    console.log("Salt: " + ns);
+    console.log("Plain text: " + plainText);
+
+
+
+    cryptoFunctions.PBKDF2(passphrase, ns, 750, 128)
+        .then(function (key) {
+            console.log('Key hex: ' + cryptoFunctions.convertDerivedKeyToHex(key));
+            keyHolder = cryptoFunctions.convertDerivedKeyToHex(key);
+
+            return cryptoFunctions.AESEncrypt(plainText, keyHolder);
+        }).then(function (encryptedData) {
+            //console.table(encryptedData);
+            console.log(cryptoFunctions.convertWordArrayToHex(encryptedData.ciphertext));
+
+            return cryptoFunctions.AESDecrypt(encryptedData, keyHolder);
+        }).then(function (plainText) {
+            console.log('Decrypted plain text: ' + plainText);
+
+        });
+
+}
+
+function encryptDecryptSubtleCrypto() {
+    var cryptoFunctions = new CryptoFunctions();
+    cryptoFunctions.useSubtle = true;
+
+    var keyHolder;
+
+    console.log("Class Subtle Crypto -> Pass phrase: " + passphrase);
+    console.log("Salt: " + ns);
+    console.log("Plain text: " + plainText);
+
+
+
+    cryptoFunctions.PBKDF2(passphrase, ns, 750, 128)
+        .then(function (key) {
+            console.log('Key hex: ' + cryptoFunctions.convertDerivedKeyToHex(key));
+            keyHolder = cryptoFunctions.convertDerivedKeyToHex(key);
+
+            return cryptoFunctions.AESEncrypt(plainText, keyHolder);
+        }).then(function (encryptedData) {
+            //console.table(encryptedData);
+            console.log(cryptoFunctions.convertUint8ArrayToHex(encryptedData.ciphertext));
+
+            return cryptoFunctions.AESDecrypt(encryptedData, keyHolder);
+        }).then(function (plainText) {
+            console.log('Decrypted plain text: ' + plainText);
+
+        });
+
+}
+
 
 function PBKDF2(passphrase, salt) {
     return new Promise(function (resolve, reject) {
@@ -57,16 +233,32 @@ function HMACSHA256(plainText, key) {
 }
 
 function convertWordArrayToUint8Array(wordArray) {
-    var len = wordArray.words.length,
-        u8_array = new Uint8Array(len << 2),
-        offset = 0,
-        word, i;
-    for (i = 0; i < len; i++) {
-        word = wordArray.words[i];
-        u8_array[offset++] = word >> 24;
-        u8_array[offset++] = (word >> 16) & 0xff;
-        u8_array[offset++] = (word >> 8) & 0xff;
-        u8_array[offset++] = word & 0xff;
+    var words = wordArray.words;
+    var sigBytes = wordArray.sigBytes;
+
+    // Convert
+    var u8 = new Uint8Array(sigBytes);
+    for (var i = 0; i < sigBytes; i++) {
+        var byte = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+        u8[i] = byte;
     }
-    return u8_array;
+
+    return u8;
+}
+
+function convertUint8ArrayToHex(u8Array) {
+    var i;
+    var len;
+    var hex = '';
+    var c;
+
+    for (i = 0, len = u8Array.length; i < len; i += 1) {
+        c = u8Array[i].toString(16);
+        if (c.length < 2) {
+            c = '0' + c;
+        }
+        hex += c;
+    }
+
+    return hex;
 }
